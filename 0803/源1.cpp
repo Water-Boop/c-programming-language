3bc3331786a1b501547b379656f8d1a959bef64a
Water <1468497338@qq.com>
2021-08-03 18:35:26 +0800

    01

---
+++ 0803/源1.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+#include <stdbool.h>
+#include<malloc.h>
+//#define _CRT_SECURE_NO_WARNINGS
+
+int main() {
+	printf("111");
+	return 0;
+}
